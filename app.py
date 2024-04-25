ad07dc144fd61b543655a9cd37127ebbe101eb6f
gazaboo <gazaboo@pop-os.localdomain>
2024-04-25 18:56:57 +0200

    added support for city selection, improved search for email

--- app.py
+++ app.py
@@ -8,45 +8,75 @@ from processing_html import get_mail_from_url
 
 st.set_page_config(layout="wide")
 
-def create_queries(user_query):
-    queries = [] 
-    with open('./cities.json') as f:
+
+def choose_locations_to_search():
+    _, countries_data = get_countries_data()
+    countries = list(countries_data.values())
+    francophones = ['France', 'Belgique', 'Luxembourg', 'Monaco']
+    checkboxes = []
+    with st.expander('Pays et Villes'):
+        for country in countries:
+            if country in francophones:
+                checkboxes.append(st.checkbox(country, value=True))
+            else:
+                checkboxes.append(st.checkbox(country))
+    countries_to_search = [country for i, country in enumerate(
+        countries) if checkboxes[i]]
+    return countries_to_search
+
+
+@st.cache_data
+def get_countries_data():
+    with open('./cities_small.json') as f:
         cities_data = json.load(f)
 
     with open('./countries.json') as f:
         countries_data = json.load(f)
 
+    return cities_data, countries_data
+
+
+def create_queries(user_query, locations):
+    queries = []
+    cities_data, countries_data = get_countries_data()
     for country_code, cities in cities_data.items():
-        for city in cities:
-            queries.append((user_query, country_code, countries_data[country_code], city))
+        if countries_data[country_code] in locations:
+            for city in cities:
+                queries.append((user_query, country_code,
+                                countries_data[country_code], city))
     return queries
 
-# Function to authenticate and create a client.
+
+@st.cache_resource
 def auth_gspread():
-    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
     google_service_account_info = st.secrets['google_service_account']
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(google_service_account_info, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        google_service_account_info, scope)
     client = gspread.authorize(creds)
     return client
 
-# Function to perform the Google search
+
 def google_search_simple_query(countryCode, query):
     url = 'https://www.googleapis.com/customsearch/v1'
     params = {
         'key': st.secrets['google_search_api_key'],
         'cx': 'd34a74997a27a4205',
-        'cr' : countryCode,
+        'cr': countryCode,
         'q': query,
     }
     response = requests.get(url, params=params)
     return response.json()
 
+
 def display_site_info(item):
+    already_listed_urls, black_list = get_listed_urls()
     st.subheader(item.get('title'))
-    if item.get('link') and item.get('link') in st.session_state.already_listed_url : 
+    if item.get('link') and item.get('link') in already_listed_urls:
         st.warning('Deja dans la base de données')
 
-    if item.get('link') and item.get('link') in st.session_state.black_list : 
+    if item.get('link') and item.get('link') in black_list:
         st.error('Dans la liste noire, ne sera pas ajoutée dans la base de données')
 
     st.write(item.get('snippet'))
@@ -55,50 +85,76 @@ def display_site_info(item):
 
 
 def add_to_spreadsheet(item, countryName, city, query):
-    if item.get('link') is not None:
-        if item.get('link') and item.get('link') not in st.session_state.already_listed_url and  item.get('link') not in st.session_state.black_list: 
-            try :
-                email = get_mail_from_url( item.get('link') )
-                st.session_state.sheet.append_row([
-                    item.get('title'),
-                    item.get('snippet'),
-                    item.get('link'), 
-                    email, 
-                    countryName, 
-                    city, 
-                    query
-                ])
-            except Exception as e:
-                print(e)
+    sheet = get_spreadsheet().sheet1
+    already_listed_urls, black_list = get_listed_urls()
+    item_link = item.get('link')
+
+    if item_link is None:
+        print('No link for --> ', item)
+        return
+
+    if item_link not in already_listed_urls and item_link not in black_list:
+        try:
+            email = get_mail_from_url(item.get('link'))
+            sheet.append_row([
+                item.get('title'),
+                item.get('snippet'),
+                item.get('link'),
+                email,
+                countryName,
+                city,
+                query
+            ])
+        except Exception as e:
+            print('-----')
+            print('Error adding to spreadsheet')
+            print(e)
 
 
-# Streamlit user interface
-def main():
-
-    # init everything
+@st.cache_resource(ttl=15)
+def get_spreadsheet():
     client = auth_gspread()
-    st.session_state['sheet'] = client.open("base_de_donnees").sheet1 
-    st.session_state['data'] = st.session_state.sheet.get_all_records()    
-    st.session_state['already_listed_url'] = set([item['URL'] for item in st.session_state.data])
-    black_list = client.open("base_de_donnees").get_worksheet(1).get_all_values()
-    st.session_state['black_list'] = set([ elt[0] for elt in black_list[1:]])
+    return client.open("base_de_donnees")
 
+
+@st.cache_resource(ttl=15)
+def get_listed_urls():
+    sheet = get_spreadsheet().sheet1
+    data = sheet.get_all_records()
+    already_listed = set([item['URL'] for item in data])
+    black_list = sheet = get_spreadsheet().get_worksheet(1).get_all_values()
+    black_list = set([elt[0] for elt in black_list[1:]])
+    return already_listed, black_list
+
+
+def display_title_logo():
     with st.sidebar:
         st.image('./logo.png', width=200)
-
-    st.title("Recherches entreprises locations de structures gonflables")
+    st.title("Recherches entreprises lo cations de structures gonflables")
     st.subheader("Création de la base de données")
 
-    user_query = st.text_input("Enter the search query")
+
+def main():
+    display_title_logo()
+    user_query = st.text_input("Recherche google")
+    locations = choose_locations_to_search()
     if st.button("Search") and user_query:
-        compound_queries = create_queries(user_query)
+        compound_queries = create_queries(user_query, locations)
+        total_elements = len(compound_queries*10)
+        progress_bar = st.progress(1/total_elements, text='Searching')
+        index = 1
         for compound_query in compound_queries:
-            query, countryCode, countryName,city = compound_query
+            query, countryCode, countryName, city = compound_query
             st.subheader(" - ".join([query,  city, countryName]))
             with st.expander("Resultats"):
-                results = google_search_simple_query(countryCode=countryCode, query=query + " " + city).get("items", [])
+                results = google_search_simple_query(
+                    countryCode=countryCode, query=query + " " + city).get("items", [])
                 for item in results:
+                    progress_bar.progress(
+                        index/total_elements, text=f'Recherche : {index} / {total_elements} ({city} - {countryName})')
                     display_site_info(item)
                     add_to_spreadsheet(item, countryName, city, query)
+                    index += 1
+
 
 main()
--- processing_html.py
+++ processing_html.py
@@ -1,12 +1,17 @@
-import re 
+import re
 import requests
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
+
 def extract_emails_from_html(html_content):
     email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     emails = re.findall(email_pattern, html_content)
-    return emails
+    wrong_emails = ['prestashop']
+    filtered_emails = [email for email in emails if not any(
+        substring in email for substring in wrong_emails)]
+    return filtered_emails
+
 
 def download_webpage(url):
     try:
@@ -19,36 +24,50 @@ def download_webpage(url):
         print(e)
         return None
 
+
 def get_mail_from_url(url):
-    try :
+    try:
         html = download_webpage(url)
 
-        if html is not None :
-            email = extract_emails_from_html(html) 
+        if html is not None:
+            email = extract_emails_from_html(html)
             if email and len(email) > 0:
                 return email[0]
 
             links_to_visit_next = get_url_from_same_domain(url, html)
             links_to_visit_next = reorder_links(links_to_visit_next)
-            if len( links_to_visit_next) > 0:
+            if len(links_to_visit_next) > 0:
                 for link in links_to_visit_next:
                     html = download_webpage(link)
                     email = extract_emails_from_html(html)
                     if email and len(email) > 0:
                         return email[0]
-            
+
             return " "
         return " "
-    except Exception as e :
+    except Exception as e:
         print(e)
         return " "
 
 
+def relative2absolute_path(url, base_domain_parsed):
+    url_parsed = urlparse(url)
+    scheme, netloc = url_parsed.scheme, url_parsed.netloc
+    if len(scheme) == 0 and len(netloc) == 0:
+        url_parsed = url_parsed._replace(scheme=base_domain_parsed.scheme)
+        url_parsed = url_parsed._replace(netloc=base_domain_parsed.netloc)
+    return url_parsed.geturl()
+
+
 def get_url_from_same_domain(url, html):
-    base_domain = urlparse(url).netloc
+    base_domain_parsed = urlparse(url)
     links = extract_all_links(html)
-    links_in_same_domain = [link for link in links if urlparse(url).netloc == base_domain]
+    links = list(map(lambda link: relative2absolute_path(
+        link, base_domain_parsed), links))
+    links_in_same_domain = [
+        link for link in links if urlparse(link).netloc == base_domain_parsed.netloc]
     return links_in_same_domain
+
 
 def extract_all_links(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -59,6 +78,6 @@ def extract_all_links(html):
             links.append(href)
     return links
 
-def reorder_links(links):
-    return list(sorted(links, key= lambda x: "contact" not in x.lower()))
 
+def reorder_links(links):
+    return list(sorted(links, key=lambda x: "contact" not in x.lower()))
